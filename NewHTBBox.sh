2d6cc696a9720d7be7c62d0d3d3ad59821dff50a
BaileyBelisario <bailey.belisario@trojans.dsu.edu>
2020-02-18 22:00:39 -0600

    added cool box script

---
+++ NewHTBBox.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+printf "Box Name: "
+read dir
+
+printf "IP: "
+read ip
+
+printf "\nMaking directory '$dir' in '~/Documents/htb/boxes'..."
+
+cd ~/Documents/htb/boxes
+mkdir $dir
+cd $dir
+
+sleep .5
+
+printf "\nRunning nmap scan on $dir"
+
+nmap -sV -sC -T5 -oA $dir $ip -p 1-65535 > /dev/null 2>&1 &
+pid=$!
+flag=0
+counter=0
+
+while [ $flag -eq 0 ]
+do
+	if [[ $(ps -p $pid | grep $pid | cut -d ' ' -f2) == $pid || $(ps -p $pid | grep $pid | cut -d ' ' -f3) == $pid ]]
+	then
+		if [ $counter -lt 5 ]
+		then
+			sleep .5
+			printf "."
+			((counter++))
+		fi
+	else
+		flag=1
+	fi
+done
+
+sleep 1
+
+printf "\n\nNmap Scan Complete!\nScript finished!\n"
